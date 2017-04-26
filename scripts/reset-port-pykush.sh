f33a03f29465a4ad13156d129bd96f0b7b9c7ea7
Tyler Baker <forcedinductionz@gmail.com>
2017-04-26 14:22:24 -0500

    scripts: reset: add error handling logic
    Signed-off-by: Tyler Baker <forcedinductionz@gmail.com>

--- scripts/reset-port-pykush.sh
+++ scripts/reset-port-pykush.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 pykush -s YK22916 -d $1
+while [ $? -ne 0 ]; do
+	echo "Disable port failed, retrying..."
+	pykush -s YK22916 -d $1
+done
 sleep 10
 pykush -s YK22916 -u $1
+while [ $? -ne 0 ]; do
+	echo "Enable port failed, retrying..."
+	pykush -s YK22916 -u $1
+done
 sleep 10
