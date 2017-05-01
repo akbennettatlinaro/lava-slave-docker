3fd58b9c32717ec6a937ef734cbf80f4ec2881f2
Tyler Baker <forcedinductionz@gmail.com>
2017-05-01 16:10:59 -0500

    scripts/pykush: add better timings
    Signed-off-by: Tyler Baker <forcedinductionz@gmail.com>

--- scripts/reset-port-pykush.sh
+++ scripts/reset-port-pykush.sh
@@ -4,10 +4,10 @@ while [ $? -ne 0 ]; do
 	echo "Disable port failed, retrying..."
 	flock /var/lock/lava-pykush.lck pykush -s YK22916 -d $1
 done
-sleep 10
+sleep 5
 flock /var/lock/lava-pykush.lck pykush -s YK22916 -u $1
 while [ $? -ne 0 ]; do
 	echo "Enable port failed, retrying..."
 	flock /var/lock/lava-pykush.lck pykush -s YK22916 -u $1
 done
-sleep 10
+sleep 20
